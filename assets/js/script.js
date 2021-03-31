646a0afc26a9c62c21fecbb89890bb94bcbec956
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-03-31 12:19:32 -0500

    add/ Added reading of radio button input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 // Poetry Project - script.js
 
 // Global Definitions
+let searchType = "";
 
 // Element Selectors
 let mySearchBtnEl = document.querySelector(".my-search-button");
+let myRadioEl = document.getElementsByName("radio-button");
 
 // Function Calls
 
 // Function Defintions
 function getUserInput() {
     console.log("I got here");
+
+    // loop through radio buttons and look for checked button
+    for (let i = 0; i < myRadioEl.length; i++) {
+
+        if(myRadioEl[i].checked) {
+            searchType = myRadioEl[i].value;
+        }
+    }
+
     
 }
 
