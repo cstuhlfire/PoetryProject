2ee17fa9ea832b4b4e284b7b03eab51163ac5465
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-03-31 22:11:20 -0500

    deleted the global object and replaced with a local object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Poetry Project - script.js
 
 // Global Definitions
-let dataObject={};
+
 
 // Element Selectors
 let mySearchBtnEl = document.querySelector(".my-search-button");
@@ -17,8 +17,8 @@ function fetchByAuthor(input) {
         fetch(apiSearch)
         .then(response => response.json())
         .then(data => {
-            let dataObject=data;
-            console.log(dataObject);
+            
+            displayPoem(data);
 
         });
 
@@ -30,18 +30,24 @@ function fetchByTitle(input) {
     fetch(apiSearch)
     .then(response => response.json())
     .then(data => {
-        console.log(data)  
+        
+        displayPoem(data);
     });
 }
 
 function chooseFetch(search, input) {
     if (search === "author") {
         fetchByAuthor(input);
-        console.log("fetch author");
+        
     } else {
         fetchByTitle(input);
-        console.log("fetch title");
+        
     }
+}
+
+function displayPoem(myObject){
+
+console.log(myObject);
 }
 
 
@@ -49,7 +55,7 @@ function getUserInput() {
     let searchType = "";
     let textInput = "";
 
-    console.log("I got here");
+    
 
     // loop through radio buttons and look for checked button
     for (let i = 0; i < myRadioEl.length; i++) {
@@ -61,7 +67,11 @@ function getUserInput() {
 
     textInput = (myInputEl.value);
     chooseFetch(searchType, textInput);
+    
+    
 }
+
+
 
 // Event Handlers
 mySearchBtnEl.addEventListener("click", getUserInput);
@@ -70,6 +80,6 @@ mySearchBtnEl.addEventListener("click", getUserInput);
 
 // read and store the input from the radio button***
 // read and store the typed text for search***
-// On submit, fetches by author or by title
+// On submit, fetches by author or by title***
 // If more than one result, display modal with choices.
 // If just one result display poem
